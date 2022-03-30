94635da195028e39261dc6e613830e6c2f300ba2
kmj4789 <81201559+kmj4789@users.noreply.github.com>
2022-03-31 03:32:13 +1300

    test

--- app.js
+++ app.js
@@ -2,14 +2,15 @@
 
 const express = require('express');
 const connectDB = require('./config/db');
+var cors = require('cors');
+
+// routes
+const books = require('./routes/api/books');
 
 const app = express();
 
+// Connect Database
 connectDB();
 
-app.get('/', (req, res) => res.send('Hello world!'));
-
-const port = process.env.PORT || 8082;
-
-app.listen(port, () => console.log(`Server running on port ${port}`));
-
+// cors
+app.use(cors({ origin: true, credentials: true }));
